99e4116761170f2a87ddf3b104bf0cd7bfa2be81
Drew Moseley <drew@moseleynet.net>
2025-08-12 12:15:04 -0400

    Store uptane-sign in ostree
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- files/scripts/install-uptane-sign.sh
+++ files/scripts/install-uptane-sign.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 URL="https://github.com/uptane/ota-tuf/releases/download/v3.2.14/cli-3.2.14.tgz"
 echo "Downloading ${URL}"
 curl -fL "$URL" -o /tmp/cli.tgz
-tar -C /opt -xzf /tmp/cli.tgz
+tar -C /usr/lib/opt -xzf /tmp/cli.tgz
 rm -f /tmp/cli.tgz
 
 echo "Installed: $(command -v uptane-sign)"
